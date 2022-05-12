e57ded0cba885592f1b33a82d0af92787e68fd1f
aramstyle <aramstyle@gmail.com>
2022-05-12 19:20:46 +0900

    첫 커밋 후 동작 되는지 확인함 => 동작ok

--- lib/src/control/count_contol_with_getx.dart
+++ lib/src/control/count_contol_with_getx.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+// TODO: 수정해야되요 그렇죠?
 class CountControlWithGetx extends GetxController {
   int count = 0;
   void increase(){
